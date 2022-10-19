a09c0681ce77bdb46cc649c9a6ffb66cf8332c2f
Admin <priyanshuaggarwal2610@gmail.com>
2022-10-20 01:28:16 +0530

    Update Push

--- src/main/java/com/springrest/springrest/dao/CommentRepo.java
+++ src/main/java/com/springrest/springrest/dao/CommentRepo.java
@@ -9,10 +9,8 @@ import java.util.List;
 
 public interface CommentRepo extends JpaRepository<Comment,Integer> {
 
-
-	//find Comments by tutorial Id
-
-      List<Comment> findByTutorial(Tutorial tutorial);
+	//find Comments by tutorial id
+          List<Comment> findByTutorial(Tutorial tutorial);
 
 	//find same words content
 	List<Comment> findByContentContaining(String words);
